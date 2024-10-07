c6186b8b1519ff25c912db4b577fe804707e8378
blakehulett7 <blake.hulett7@gmail.com>
2024-10-07 16:35:19 -0500

    Added more assertions to make sure the in memory entries are of the correct length!

--- installer.go
+++ installer.go
@@ -13,6 +13,10 @@ import (
 const storageDirectory = "fmrfastforward"
 const dbPath = storageDirectory + "/sql.db"
 const apiHeader = "speedrun bot, email: blake.hulett7@gmail.com"
+const known_deck_table_length = 3649
+const known_sapow_table_length = 3066
+const known_satec_table_length = 2917
+const known_bcd_table_length = 2683
 
 func generateApiUrl(pagetoFetch string) string {
 	return fmt.Sprintf("https://yugipedia.com/api.php?action=query&prop=revisions&titles=%v&rvprop=content&format=json", pagetoFetch)
@@ -66,18 +70,21 @@ func getFmrData() {
 	charactersQuery := read_character_data()
 	wikimap := charactersQuery.Query.Pages
 	deck_entries, sapow_entries, satec_entries, bcd_entries := parse_wikitext(wikimap)
+	assert(len(deck_entries) == known_deck_table_length, "incorrect number of deck entries, most likely missing cards...")
+	assert(len(sapow_entries) == known_sapow_table_length, "incorrect number of sapow entries, most likely missing cards...")
+	assert(len(satec_entries) == known_satec_table_length, "incorrect number of satec entries, most likely missing cards...")
+	assert(len(bcd_entries) == known_bcd_table_length, "incorrect number of bcd entries, most likely missing cards...")
 	assert(tableIsEmpty("decks"), "there is old data in the decks table")
 	WriteProbabilities(deck_entries, "decks")
-	assert(!tableIsEmpty("decks"), "decks table not successfully written")
-	assert(tableIsEmpty("sapow"), "there is old data in the sapow table")
+	assert(table_has_length("decks", known_deck_table_length), "deck table incorrectly written, we are missing cards most likely...")
 	WriteProbabilities(sapow_entries, "sapow")
-	assert(!tableIsEmpty("sapow"), "sapow table not successfully written")
+	assert(table_has_length("sapow", known_sapow_table_length), "sapow table incorrectly written, we are missing cards most likely...")
 	assert(tableIsEmpty("satec"), "there is old data in the satec table")
 	WriteProbabilities(satec_entries, "satec")
-	assert(!tableIsEmpty("satec"), "satec table not successfully written")
+	assert(table_has_length("satec", known_satec_table_length), "satec table incorrectly written, we are missing cards most likely...")
 	assert(tableIsEmpty("bcd"), "there is old data in the bcd table")
 	WriteProbabilities(bcd_entries, "bcd")
-	assert(!tableIsEmpty("bcd"), "bcd table not successfully written")
+	assert(table_has_length("bcd", known_bcd_table_length), "bcd table incorrectly written, we are missing cards most likely...")
 }
 
 func getFmrCharacters() {
