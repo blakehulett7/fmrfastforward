e6a46a6fcf320e76beb09afc70b926a66eb63ee5
blakehulett7 <blake.hulett7@gmail.com>
2024-10-21 19:33:01 -0500

    Quick simplifier!

--- unit_test.go
+++ unit_test.go
@@ -8,9 +8,7 @@ import (
 func TestInstaller(t *testing.T) {
 	if fileExists(dbPath) {
 		os.Rename(dbPath, dbHoldingPath)
-		t.Cleanup(func() {
-			os.Rename(dbHoldingPath, dbPath)
-		})
+		defer os.Rename(dbHoldingPath, dbPath)
 	}
 	defer os.Remove(dbPath)
 	install(dbPath)
