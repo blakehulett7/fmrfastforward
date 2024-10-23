3541cfba4538fd7cba2d91fe692ffaa1d79c2149
blakehulett7 <blake.hulett7@gmail.com>
2024-10-23 13:39:47 -0500

    Newbie code??

--- simulation.go
+++ simulation.go
@@ -15,11 +15,28 @@ func (sim *simulation) increment_seed() {
 }
 
 func (sim simulation) generate_starting_deck() {
+	cards_to_get := []string{}
+
 	pool_sub_1100 := get_starting_deck_rates("pool_sub_1100")
 	pool_1100_1600 := get_starting_deck_rates("pool_1100_1600")
+	pool_1600_2100 := get_starting_deck_rates("pool_1600_2100")
+	pool_over_2100 := get_starting_deck_rates("pool_over_2100")
+	pool_pure_magic := get_starting_deck_rates("pool_pure_magic")
+	pool_field_magic := get_starting_deck_rates("pool_field_magic")
+	pool_equip_magic := get_starting_deck_rates("pool_equip_magic")
 
-	fmt.Println(sim.get_cards_from_pool(pool_sub_1100, 16))
-	fmt.Println(sim.get_cards_from_pool(pool_1100_1600, 16))
+	cards_to_get = append(cards_to_get, sim.get_cards_from_pool(pool_sub_1100, 16)...)
+	cards_to_get = append(cards_to_get, sim.get_cards_from_pool(pool_1100_1600, 16)...)
+
+	cards_to_get = append(cards_to_get, sim.get_cards_from_pool(pool_1600_2100, 4)...)
+
+	cards_to_get = append(cards_to_get, sim.get_cards_from_pool(pool_over_2100, 1)...)
+	cards_to_get = append(cards_to_get, sim.get_cards_from_pool(pool_pure_magic, 1)...)
+	cards_to_get = append(cards_to_get, sim.get_cards_from_pool(pool_field_magic, 1)...)
+	cards_to_get = append(cards_to_get, sim.get_cards_from_pool(pool_equip_magic, 1)...)
+
+	assert(len(cards_to_get) == 40, "bug generating the starting deck, did not get exactly 40 cards to grab from the db")
+	fmt.Println(cards_to_get)
 }
 
 func (sim *simulation) drop_card(drop_table []Probability) (card_name string) {
