271b64a128086b31c4845174b5e8a4abe61160f7
blakehulett7 <blake.hulett7@gmail.com>
2024-10-26 14:35:36 -0500

    I think we have some naive odds successfully calculated!

--- calculations.go
+++ calculations.go
@@ -1,5 +1,18 @@
 package main
 
-func odds_of_drawing_fusion(fusion Fusion) {
+import "github.com/montanaflynn/stats"
 
+func odds_of_drawing_fusion(fusion Fusion, draws int) float64 {
+	ways_to_draw_an_m1 := ways_to_draw_at_least_n_cards(len(fusion.m1_components), draws)
+	ways_to_draw_an_m2 := ways_to_draw_at_least_n_cards(len(fusion.m2_components), draws)
+	possible_hand_combinations := stats.Ncr(40, draws)
+	return (float64(ways_to_draw_an_m1) / float64(possible_hand_combinations)) * (float64(ways_to_draw_an_m2) / float64(possible_hand_combinations))
+}
+
+func ways_to_draw_at_least_n_cards(n, draws int) int {
+	ways_to_draw := 0
+	for i := 1; i <= n; i++ {
+		ways_to_draw += stats.Ncr(n, i) * stats.Ncr((40-n), (draws-i))
+	}
+	return ways_to_draw
 }
--- logic.go
+++ logic.go
@@ -32,49 +32,45 @@ func evaluate_starting_deck(starting_deck []Card) {
 					if !slices.Contains(fusion_map_m2[fusion], target_card.Name) {
 						fusion_map_m2[fusion] = append(fusion_map_m2[fusion], target_card.Name)
 					}
-
-					/*
-
-						fusion := Fusion{
-							Name:          fusion_card.Name,
-							Attack:        fusion_card.Attack,
-							Defense:       fusion_card.Defense,
-							fusion_number: num,
-							m1:            card.Name,
-							m2:            target_card.Name,
-						}
-					*/
 				}
 			}
 		}
 	}
 
-	fmt.Println(fusion_map_m1)
-	fmt.Println(fusion_map_m2)
+	//fmt.Println(fusion_map_m1)
+	//fmt.Println(fusion_map_m2)
 
 	fusions := []Fusion{}
 	for fusion, m1_components := range fusion_map_m1 {
 		m2_components := fusion_map_m2[fusion]
-		fmt.Printf("Fusion: %v, m1's: %v, m2's: %v\n", fusion, m1_components, m2_components)
+		//fmt.Printf("Fusion: %v, m1's: %v, m2's: %v\n", fusion, m1_components, m2_components)
 
 		fusion_card := get_card(strings.Split(fusion, "|")[0]) //Huge performace hit here just fyi, solution in concurrency?
 		num, _ := strconv.Atoi(strings.Split(fusion, "|")[1])
 		assert(num > 0, "bad conversion")
+
+		fusion := Fusion{
+			Name:          fusion_card.Name,
+			Attack:        fusion_card.Attack,
+			Defense:       fusion_card.Defense,
+			fusion_number: num,
+			m1_components: m1_components,
+			m2_components: m2_components,
+		}
+
+		fusions = append(fusions, fusion)
 	}
 
 	fusions_by_atk := fusions
-	/*
-		slices.SortFunc(fusions_by_atk, func(a Fusion, b Fusion) int {
-			return b.Attack - a.Attack
-		})
-	*/
-	var chance_to_draw float64 = 2.0
+
+	slices.SortFunc(fusions_by_atk, func(a Fusion, b Fusion) int {
+		return b.Attack - a.Attack
+	})
+
+	var chance_to_draw float64
 	var percent_chance float64
 	for _, card := range fusions_by_atk {
-		chance_to_not_draw := 40 - chance_to_draw
-		first_hand_percent_change_to_draw := (1 - ((chance_to_not_draw / 40.0) * ((chance_to_not_draw - 1) / 39.0))) * 100
-		percent_chance += first_hand_percent_change_to_draw
-		fmt.Println(card.Name, card.Attack, card.fusion_number, card.m1, card.m2)
+		fmt.Println(card.Name, card.Attack, card.fusion_number, card.m1_components, card.m2_components, odds_of_drawing_fusion(card, 5))
 	}
 	fmt.Println()
 
