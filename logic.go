2e50cb8443b3263929149aa50bd5b8b86ff59fee
blakehulett7 <blake.hulett7@gmail.com>
2024-10-28 13:26:34 -0500

    Taking a checkpoint, working on java.

--- logic.go
+++ logic.go
@@ -10,68 +10,10 @@ import (
 func evaluate_starting_deck(starting_deck [40]Card) {
 	assert(len(starting_deck) == 40, "Decks must have exactly 40 cards")
 
-	card_counts := count_my_cards(starting_deck)
-
-	cards := starting_deck[:]
-
-	fusion_map_m1 := map[string][]string{}
-	fusion_map_m2 := map[string][]string{}
-	for _, card := range cards {
-		for _, target_card := range cards {
-			for _, fusion := range card.m1_potential {
-				if slices.Contains(target_card.m2_potential, fusion) {
-					//fmt.Printf("Found fusion! m1: %v, m2: %v, result: %v\n", card.Name, target_card.Name, fusion)
-					if !slices.Contains(fusion_map_m1[fusion], card.Name) {
-						count := card_counts[card.Name]
-						for i := 0; i < count; i++ {
-							fusion_map_m1[fusion] = append(fusion_map_m1[fusion], card.Name)
-						}
-					}
-
-					if !slices.Contains(fusion_map_m2[fusion], target_card.Name) {
-						count := card_counts[target_card.Name]
-						for i := 0; i < count; i++ {
-							fusion_map_m2[fusion] = append(fusion_map_m2[fusion], target_card.Name)
-						}
-					}
-				}
-			}
-		}
-	}
-
-	//fmt.Println(fusion_map_m1)
-	//fmt.Println(fusion_map_m2)
-
-	fusions := []Fusion{}
-	for fusion, m1_components := range fusion_map_m1 {
-
-		m2_components := fusion_map_m2[fusion]
-		//fmt.Printf("Fusion: %v, m1's: %v, m2's: %v\n", fusion, m1_components, m2_components)
-
-		fusion_card := get_card(strings.Split(fusion, "|")[0]) //Huge performace hit here just fyi, solution in concurrency?
-		num, _ := strconv.Atoi(strings.Split(fusion, "|")[1])
-		assert(num > 0, "bad conversion")
-
-		fusion := Fusion{
-			Name:          fusion_card.Name,
-			Attack:        fusion_card.Attack,
-			Defense:       fusion_card.Defense,
-			fusion_number: num,
-			m1_components: m1_components,
-			m2_components: m2_components,
-		}
-
-		fusions = append(fusions, fusion)
-	}
-
-	fusions_by_atk := fusions
-
-	slices.SortFunc(fusions_by_atk, func(a Fusion, b Fusion) int {
-		return b.Attack - a.Attack
-	})
+	fusions := get_best_fusions(starting_deck)
 
 	var chance_to_draw float64
-	for _, card := range fusions_by_atk {
+	for _, card := range fusions {
 		chance_to_draw = odds_of_drawing_fusion(card, 5)
 		chance_to_draw *= 100
 		fmt.Println(card.Name, card.Attack, card.fusion_number, card.m1_components, card.m2_components, fmt.Sprintf("%.2f%%", chance_to_draw))
